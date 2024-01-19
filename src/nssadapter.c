06b6b81e6c174d83fc80ee6f4dfdca11a68e8a8a
Francisco Ferrari Bihurriet <fferrari@redhat.com>
2024-01-19 20:35:27 -0300

    Implement exported function (symbols) decoration
    Now DECLARE_DECORATOR() also works for symbols defined in a dependent
    library (shared object). DECORATE_FUNCTION_LIST() is the way to replace
    function pointers in a function list, saving the original function.
    
    SAVE_ORIGINAL_SYMBOL() looks up for an overwritten exported symbol in
    a dependent library, saving the original function for use in our own
    library. This macro is called from the library constructor and uses
    dlopen()/dlsym() to find the already loaded dependency, and resolve
    the function we want to forward calls from the wrapper/decorator.
    
    Use this mechanism to decorate FC_GetInterface and FC_GetFunctionList
    from libsoftokn3.so, using these hook points to make the function list
    pointers decorations. Implement C_GetInterface and C_GetFunctionList
    forwarding to the FIPS versions (the ones we decorated).

--- src/nssadapter.c
+++ src/nssadapter.c
@@ -2,37 +2,60 @@
 
 #include "nssadapter.h"
 #include <stdint.h>
+#include <dlfcn.h>
 #include <nss3/pkcs11.h>
 
 /* ****************************************************************************
- * PKCS #11 Decorator macros
+ * PKCS #11 and symbol decorator macros
  * ****************************************************************************/
+
+// Define a name for a pointer type that corresponds with a function
+#define FUNC_TYPE(name) name ## _PTR
 
 // Get the name of the saved original wrapped/decorated function
 #define ORIGINAL(name)  name ## _Original
 
-// Get the name of the wrapper/decorator function
-#define DECORATOR(name) name ## _Decorator
-
 // Declare a function wrapper/decorator, along with a function pointer
 // to keep a reference to the original wrapped/decorated function
 #define DECLARE_DECORATOR(ret_type, name, ...)                                 \
-    static ret_type (*ORIGINAL(name))(__VA_ARGS__) = NULL;                     \
-    ret_type DECORATOR(name)(__VA_ARGS__)
-
-// Apply a declared function wrapper/decorator to a function list
-#define DECORATE(name, pFunctionList) do {                                     \
-    ORIGINAL(name) = (pFunctionList)->name;                                    \
-    dbg_trace("Decorating " #name " (replacing " HEX64 " by " HEX64 ")",       \
-              (uintptr_t)ORIGINAL(name), (uintptr_t)DECORATOR(name));          \
-    (pFunctionList)->name = DECORATOR(name);                                   \
-} while(0)
-
-// Define a function having its NSS FIPS prototype (FC_... proto + C_... func)
-#define WITH_FIPS_PROTOTYPE(ret_type, name, ...)                               \
-    ret_type F ## name(__VA_ARGS__);                                           \
+    typedef ret_type (*FUNC_TYPE(name))(__VA_ARGS__);                          \
+    static FUNC_TYPE(name) ORIGINAL(name) = NULL;                              \
     ret_type name(__VA_ARGS__)
 
+// Apply a declared function wrapper/decorator to a function list
+#define DECORATE_FUNCTION_LIST(name, pFunctionList) do {                       \
+    ORIGINAL(name) = (pFunctionList)->name;                                    \
+    (pFunctionList)->name = name;                                              \
+    dbg_trace("Decorated " #name " (" HEX64 ", replaced by " HEX64 ")",        \
+              (uintptr_t)ORIGINAL(name), (uintptr_t)name);                     \
+} while(0)
+
+// Save the original wrapped/decorated imported symbol (to be called at load)
+#define SAVE_ORIGINAL_SYMBOL(lib, name) do {                                   \
+    ORIGINAL(name) = (FUNC_TYPE(name)) __resolve("lib" #lib ".so", #name);     \
+    dbg_trace("Saved original " #name " (" HEX64 ", replaced by " HEX64 ")",   \
+              (uintptr_t)ORIGINAL(name), (uintptr_t)name);                     \
+} while(0)
+
+// Do not call directly, use SAVE_ORIGINAL_SYMBOL(lib, name) instead
+static void (*  __resolve(const char *lib_name, const char *symbol)  )(void) {
+    void *handle = dlopen(lib_name, RTLD_NOLOAD | RTLD_LAZY | RTLD_LOCAL);
+    void *func = NULL;
+    if (handle) {
+        func = dlsym(handle, symbol);
+        dlclose(handle);
+    }
+    dbg_trace(handle ? ( func ? "Opened %s, and successfully resolved %s" :
+              "Opened %s, but could not resolve %s, " WILL_CRASH ) :
+              "Could not open %s, " WILL_CRASH, lib_name, symbol);
+    // Temporarily allow casting a data pointer to a function pointer.
+    // By returning a generic void(*f)(void) function pointer, we make
+    // sure a later cast to another function pointer will not fail.
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wpedantic"
+    return func;
+    #pragma GCC diagnostic pop
+}
 
 /* ****************************************************************************
  * Internal/Private functions
@@ -76,26 +99,14 @@ DECLARE_DECORATOR(CK_RV, C_GetAttributeValue,
  * Exported/Public functions
  * ****************************************************************************/
 
-/*
-void CONSTRUCTOR_FUNCTION library_constructor(void) {
-    // TODO: create wrapper keys for import/export workaround
-    dbg_trace();
-}
-
-void DESTRUCTOR_FUNCTION library_destructor(void) {
-    // TODO: destroy wrapper keys
-    dbg_trace();
-}
-*/
-
-// Call FIPS implementation of C_GetInterface and apply decorations
-WITH_FIPS_PROTOTYPE(CK_RV, C_GetInterface,
+DECLARE_DECORATOR(CK_RV, FC_GetInterface,
   CK_UTF8CHAR_PTR pInterfaceName,
   CK_VERSION_PTR pVersion,
   CK_INTERFACE_PTR_PTR ppInterface,
   CK_FLAGS flags
 ) {
-    CK_RV ret = FC_GetInterface(pInterfaceName, pVersion, ppInterface, flags);
+    CK_RV ret = ORIGINAL(FC_GetInterface)(pInterfaceName, pVersion,
+                                          ppInterface, flags);
     if (ret == CKR_OK) {
         // NOTE: the PKCS #11 v3.0 standard states 'CK_FUNCTION_LIST_3_0
         // is a structure which contains the same function pointers as in
@@ -105,23 +116,52 @@ WITH_FIPS_PROTOTYPE(CK_RV, C_GetInterface,
         // the version, as long as it contains all the functions we need
         // to decorate, which is our case.
         CK_FUNCTION_LIST_PTR pFunctionList = (*ppInterface)->pFunctionList;
-        DECORATE(C_CreateObject, pFunctionList);
-        DECORATE(C_GetAttributeValue, pFunctionList);
-        dbg_trace("NSS PKCS #11 v%d.%d, libsoftokn3.so successfully adapted",
+        DECORATE_FUNCTION_LIST(C_CreateObject, pFunctionList);
+        DECORATE_FUNCTION_LIST(C_GetAttributeValue, pFunctionList);
+        dbg_trace("NSS PKCS #11 v%d.%d, software token successfully adapted",
                   pFunctionList->version.major, pFunctionList->version.minor);
     }
     return ret;
 }
 
-// Call FIPS implementation of C_GetFunctionList and apply decorations
-WITH_FIPS_PROTOTYPE(CK_RV, C_GetFunctionList,
+DECLARE_DECORATOR(CK_RV, FC_GetFunctionList,
   CK_FUNCTION_LIST_PTR_PTR ppFunctionList
 ) {
-    CK_RV ret = FC_GetFunctionList(ppFunctionList);
+    CK_RV ret = ORIGINAL(FC_GetFunctionList)(ppFunctionList);
     if (ret == CKR_OK) {
-        DECORATE(C_CreateObject, *ppFunctionList);
-        DECORATE(C_GetAttributeValue, *ppFunctionList);
-        dbg_trace("NSS PKCS #11, libsoftokn3.so successfully adapted");
+        DECORATE_FUNCTION_LIST(C_CreateObject, *ppFunctionList);
+        DECORATE_FUNCTION_LIST(C_GetAttributeValue, *ppFunctionList);
+        dbg_trace("NSS PKCS #11, software token successfully adapted");
     }
     return ret;
+}
+
+// Force FIPS setup
+CK_RV C_GetInterface(
+  CK_UTF8CHAR_PTR pInterfaceName,
+  CK_VERSION_PTR pVersion,
+  CK_INTERFACE_PTR_PTR ppInterface,
+  CK_FLAGS flags
+) {
+    return FC_GetInterface(pInterfaceName, pVersion, ppInterface, flags);
+}
+
+CK_RV C_GetFunctionList(
+  CK_FUNCTION_LIST_PTR_PTR ppFunctionList
+) {
+    return FC_GetFunctionList(ppFunctionList);
+}
+
+/* ****************************************************************************
+ * Library constructor and destructor
+ * ****************************************************************************/
+
+void CONSTRUCTOR_FUNCTION library_constructor(void) {
+    SAVE_ORIGINAL_SYMBOL(softokn3, FC_GetInterface);
+    SAVE_ORIGINAL_SYMBOL(softokn3, FC_GetFunctionList);
+    // TODO: create wrapper keys for import/export workaround
+}
+
+void DESTRUCTOR_FUNCTION library_destructor(void) {
+    // TODO: destroy wrapper keys
 }
