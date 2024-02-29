ca0cdddc169c7c15b15a5f1807268cdea8d8c132
Francisco Ferrari Bihurriet <fferrari@redhat.com>
2024-02-29 20:32:49 +0100

    Code review 1st iteration, part 5
    Co-authored-by: Martin Balao <mbalao@redhat.com>

--- src/exporter.c
+++ src/exporter.c
@@ -8,9 +8,10 @@
 #include <nss3/secasn1.h>
 #include <nss3/secoid.h>
 
-// Thread-local stored exported attributes, to keep them
-// from one call (querying the buffer sizes) to the other
-// (passing the allocated buffers to get the attributes).
+// OpenJDK's libj2pkcs11 follows the "Conventions for functions returning output
+// in a variable-length buffer" (PKCS #11 v3.0 Section 5.2). Keep state between
+// querying the buffer sizes and executing the actual call in thread-local
+// variables.
 static __thread bool cached_attrs_initialized = false;
 static __thread CK_ATTRIBUTE cached_sensitive_attrs[] = {
 #define for_each_sensitive_attr(idx, sensitive_attr_type)                      \
@@ -44,20 +45,21 @@ static inline void clear_sensitive_cached_attrs(void) {
     cached_attrs_initialized = false;
 }
 
-#define __store_cached_attr(attr_type, source)                                 \
+#define __store_cached_attr(attr_type, sec_item_attr)                          \
     do {                                                                       \
         CK_ATTRIBUTE_PTR cached_attr = get_sensitive_cached_attr(attr_type);   \
         if (cached_attr == NULL) {                                             \
-            dbg_trace("Trying to store unknown sensitive attribute");          \
+            dbg_trace("Cannot store unknown sensitive attribute " #attr_type); \
             return CKR_GENERAL_ERROR;                                          \
         }                                                                      \
-        cached_attr->pValue = malloc((source).len);                            \
+        cached_attr->pValue = malloc((sec_item_attr).len);                     \
         if (cached_attr->pValue == NULL) {                                     \
             dbg_trace("Ran out of memory while exporting " #attr_type);        \
             return CKR_HOST_MEMORY;                                            \
         }                                                                      \
-        memcpy(cached_attr->pValue, (source).data, (source).len);              \
-        cached_attr->ulValueLen = (source).len;                                \
+        memcpy(cached_attr->pValue, (sec_item_attr).data,                      \
+               (sec_item_attr).len);                                           \
+        cached_attr->ulValueLen = (sec_item_attr).len;                         \
     } while (0)
 
 static CK_RV decode_and_store_secret_key(CK_BYTE_PTR *encoded_key,
@@ -71,33 +73,52 @@ static CK_RV decode_and_store_secret_key(CK_BYTE_PTR *encoded_key,
 }
 
 static CK_RV decode_and_store_private_key(CK_KEY_TYPE key_type,
-                                          PLArenaPool *arena,
-                                          SECItem *encoded_key_item) {
+                                          CK_BYTE_PTR encoded_key,
+                                          CK_ULONG encoded_key_len) {
+    CK_RV ret = CKR_OK;
+    PLArenaPool *arena = NULL;
     NSSLOWKEYPrivateKeyInfo *pki;
     NSSLOWKEYPrivateKey *lpk;
+
+    if (encoded_key_len > UINT_MAX) {
+        dbg_trace("Too big encoded key (%lu bytes)", encoded_key_len);
+        return_with_cleanup(CKR_GENERAL_ERROR);
+    }
+    SECItem encoded_key_item = {.type = siBuffer,
+                                .data = encoded_key,
+                                .len = (unsigned int)encoded_key_len};
+
+    arena = PORT_NewArena(2048);
+    if (arena == NULL) {
+        return_with_cleanup(CKR_HOST_MEMORY);
+    }
     if (!allocate_PrivateKeyInfo_and_PrivateKey(arena, &pki, &lpk)) {
-        return CKR_HOST_MEMORY;
+        return_with_cleanup(CKR_HOST_MEMORY);
     }
 
     if (SEC_QuickDERDecodeItem(arena, pki, nsslowkey_PrivateKeyInfoTemplate,
-                               encoded_key_item) != SECSuccess) {
+                               &encoded_key_item) != SECSuccess) {
         dbg_trace("Failed to decode PKCS #8 private key");
-        return CKR_GENERAL_ERROR;
+        return_with_cleanup(CKR_GENERAL_ERROR);
     }
     SECOidTag alg_tag = SECOID_GetAlgorithmTag(&pki->algorithm);
     switch (key_type) {
     case CKK_RSA:
+        // We only care about sensitive attributes. For this reason,
+        // SEC_OID_PKCS1_RSA_PSS_SIGNATURE does not need algorithm
+        // parameters handling to extract the CKA_PUBLIC_KEY_INFO
+        // value: P11.C_GetAttributeValue() already did so.
         if (alg_tag != SEC_OID_PKCS1_RSA_ENCRYPTION &&
             alg_tag != SEC_OID_PKCS1_RSA_PSS_SIGNATURE) {
             dbg_trace("Unexpected key algorithm tag: %u", alg_tag);
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
         lpk->keyType = NSSLOWKEYRSAKey;
         prepare_low_rsa_priv_key_for_asn1(lpk);
         if (SEC_QuickDERDecodeItem(arena, lpk, nsslowkey_RSAPrivateKeyTemplate,
                                    &pki->privateKey) != SECSuccess) {
             dbg_trace("Failed to decode PKCS #8 RSA private key");
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
         __store_cached_attr(CKA_PRIVATE_EXPONENT, lpk->u.rsa.privateExponent);
         __store_cached_attr(CKA_PRIME_1, lpk->u.rsa.prime1);
@@ -108,35 +129,39 @@ static CK_RV decode_and_store_private_key(CK_KEY_TYPE key_type,
         dbg_trace("Successfully decoded RSA private key");
         break;
     case CKK_DSA:
+        // We only care about sensitive attributes. For this reason,
+        // we don't need to decode the PQG parameters to extract the
+        // CKA_PRIME, CKA_SUBPRIME and CKA_BASE attribute values:
+        // P11.C_GetAttributeValue() already did so.
         if (alg_tag != SEC_OID_ANSIX9_DSA_SIGNATURE) {
             dbg_trace("Unexpected key algorithm tag: %u", alg_tag);
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
-        lpk->keyType = NSSLOWKEYDSAKey;
-        prepare_low_dsa_priv_key_export_for_asn1(lpk);
-
         lpk->keyType = NSSLOWKEYDSAKey;
         prepare_low_dsa_priv_key_export_for_asn1(lpk);
         if (SEC_QuickDERDecodeItem(arena, lpk,
                                    nsslowkey_DSAPrivateKeyExportTemplate,
                                    &pki->privateKey) != SECSuccess) {
             dbg_trace("Failed to decode PKCS #8 DSA private key");
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
         __store_cached_attr(CKA_VALUE, lpk->u.dsa.privateValue);
         dbg_trace("Successfully decoded DSA private key");
         break;
     case CKK_EC:
+        // We only care about sensitive attributes. For this reason, we don't
+        // need to copy lpk->u.ec.ecParams.DEREncoding to set the CKA_EC_PARAMS
+        // attribute value: P11.C_GetAttributeValue() already did so.
         if (alg_tag != SEC_OID_ANSIX962_EC_PUBLIC_KEY) {
             dbg_trace("Unexpected key algorithm tag: %u", alg_tag);
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
         lpk->keyType = NSSLOWKEYECKey;
         prepare_low_ec_priv_key_for_asn1(lpk);
         if (SEC_QuickDERDecodeItem(arena, lpk, nsslowkey_ECPrivateKeyTemplate,
                                    &pki->privateKey) != SECSuccess) {
             dbg_trace("Failed to decode PKCS #8 EC private key");
-            return CKR_GENERAL_ERROR;
+            return_with_cleanup(CKR_GENERAL_ERROR);
         }
         __store_cached_attr(CKA_VALUE, lpk->u.ec.privateValue);
         dbg_trace("Successfully decoded EC private key");
@@ -145,16 +170,20 @@ static CK_RV decode_and_store_private_key(CK_KEY_TYPE key_type,
         dbg_trace("This should never happen, given is_importable_exportable() "
                   "was previously called\n  key_type = " CKK_FMT,
                   key_type);
-        return CKR_GENERAL_ERROR;
+        return_with_cleanup(CKR_GENERAL_ERROR);
     }
-    return CKR_OK;
+
+cleanup:
+    if (arena != NULL) {
+        PORT_FreeArena(arena, /* zero = */ PR_TRUE);
+    }
+    return ret;
 }
 
-static CK_RV export_and_store_key_in_tls(CK_OBJECT_CLASS key_class,
-                                         CK_KEY_TYPE key_type,
-                                         CK_OBJECT_HANDLE key_id) {
+static CK_RV export_and_store_key(CK_OBJECT_CLASS key_class,
+                                  CK_KEY_TYPE key_type,
+                                  CK_OBJECT_HANDLE key_id) {
     CK_RV ret = CKR_OK;
-    PLArenaPool *arena = NULL;
     CK_BYTE_PTR encoded_key = NULL;
     CK_ULONG encoded_key_len = 0;
     CK_BYTE_PTR encrypted_key = NULL;
@@ -189,18 +218,8 @@ static CK_RV export_and_store_key_in_tls(CK_OBJECT_CLASS key_class,
     if (key_class == CKO_SECRET_KEY) {
         ret = decode_and_store_secret_key(&encoded_key, encoded_key_len);
     } else if (key_class == CKO_PRIVATE_KEY) {
-        if (encoded_key_len > UINT_MAX) {
-            dbg_trace("Too big encoded key (%lu bytes)", encoded_key_len);
-            return_with_cleanup(CKR_GENERAL_ERROR);
-        }
-        SECItem encoded_key_item = {.type = siBuffer,
-                                    .data = encoded_key,
-                                    .len = (unsigned int)encoded_key_len};
-        arena = PORT_NewArena(2048);
-        if (arena == NULL) {
-            return_with_cleanup(CKR_HOST_MEMORY);
-        }
-        ret = decode_and_store_private_key(key_type, arena, &encoded_key_item);
+        ret = decode_and_store_private_key(key_type, encoded_key,
+                                           encoded_key_len);
     } else {
         dbg_trace("This should never happen, given is_importable_exportable() "
                   "was previously called\n  key_class = " CKO_FMT,
@@ -218,9 +237,6 @@ cleanup:
     if (encoded_key != NULL) {
         zeroize_and_free(encoded_key, encoded_key_len);
     }
-    if (arena != NULL) {
-        PORT_FreeArena(arena, /* zero = */ PR_TRUE);
-    }
     return ret;
 }
 
@@ -229,34 +245,33 @@ CK_RV export_key(CK_OBJECT_CLASS key_class, CK_KEY_TYPE key_type,
                  CK_ATTRIBUTE_PTR attributes, CK_ULONG n_attributes) {
     CK_RV ret = CKR_OK;
 
-    // Check convention described in PKCS #11 v3.0 Section 5.2 on producing
-    // output. We know libj2pkcs11 OpenJDK native library uses this convention.
+    // Consistency checks for convention described in PKCS #11 v3.0 Section 5.2.
     for (size_t n = 0; n < n_attributes; n++) {
         CK_ATTRIBUTE_PTR cached_attr =
             get_sensitive_cached_attr(attributes[n].type);
         if (cached_attr == NULL) {
-            // Skip known non-sensitive attribute.
+            // Skip non-sensitive attribute.
             continue;
         }
         if (!cached_attrs_initialized && attributes[n].pValue != NULL) {
             dbg_trace_attr("First call should query the buffer sizes, "
-                           "offending attribute",
+                           "unexpected attribute",
                            attributes[n]);
             return_with_cleanup(CKR_GENERAL_ERROR);
         }
         if (cached_attrs_initialized) {
             if (attributes[n].pValue == NULL) {
                 dbg_trace_attr("Second call should have allocated the buffers, "
-                               "offending attribute",
+                               "unexpected attribute",
                                attributes[n]);
                 return_with_cleanup(CKR_GENERAL_ERROR);
             }
             if (cached_attr->pValue == NULL ||
                 cached_attr->ulValueLen != attributes[n].ulValueLen) {
                 dbg_trace_attr("Cached attribute and destination attribute "
-                               "data mismatch, offending attribute",
+                               "data mismatch, unexpected attribute",
                                attributes[n]);
-                dbg_trace_attr("Corresponding cached attribute", *cached_attr);
+                dbg_trace_attr("Cached attribute counterpart", *cached_attr);
                 return_with_cleanup(CKR_GENERAL_ERROR);
             }
         }
@@ -267,18 +282,26 @@ CK_RV export_key(CK_OBJECT_CLASS key_class, CK_KEY_TYPE key_type,
               "key_id = %lu, attributes = %p, n_attributes = %lu, "
               "ret = " CKR_FMT,
               session, key_id, (void *)attributes, n_attributes, ret);
-    if (dbg_is_enabled()) {
+
+    if (dbg_is_enabled() && ret != CKR_ATTRIBUTE_SENSITIVE) {
+        // For CKR_ATTRIBUTE_SENSITIVE we have more detailed logging.
         for (size_t n = 0; n < n_attributes; n++) {
             dbg_trace_attr("Attribute returned by NSS C_GetAttributeValue()",
                            attributes[n]);
         }
     }
+
     if (ret == CKR_OK) {
+        // All the attributes are non-sensitive
         if (cached_attrs_initialized) {
             dbg_trace("Sensitive attributes were expected");
             return_with_cleanup(CKR_GENERAL_ERROR);
         }
         if (n_attributes >= 3) {
+            // OpenJDK may query these three attributes to determine if
+            // the key is opaque. Based on our FIPS configuration (FIPS
+            // enabled and no-DB), these attribute values (if present)
+            // must be token == CK_FALSE and sensitive == CK_TRUE.
             CK_BBOOL *token = NULL;
             CK_BBOOL *sensitive = NULL;
             CK_BBOOL *extractable = NULL;
@@ -295,41 +318,44 @@ CK_RV export_key(CK_OBJECT_CLASS key_class, CK_KEY_TYPE key_type,
                     } else if (attributes[n].type == CKA_SENSITIVE) {
                         sensitive = attributes[n].pValue;
                         if (*sensitive == CK_FALSE) {
-                            // This should never happen in FIPS mode, given that
+                            // This should never happen in FIPS mode given that
                             // is_importable_exportable() returned true, so the
                             // key is secret or private.
-                            dbg_trace("Non-sensitive key, the exporter is "
-                                      "unnecessary");
-                            break;
+                            dbg_trace("Non-sensitive key, this is unexpected "
+                                      "in FIPS mode");
+                            return_with_cleanup(CKR_GENERAL_ERROR);
                         }
                     } else if (attributes[n].type == CKA_EXTRACTABLE) {
                         extractable = attributes[n].pValue;
-                        if (*extractable == CK_FALSE) {
-                            dbg_trace("Non-extractable key, the exporter will "
-                                      "not work");
-                            break;
-                        }
                     }
                 }
-                if (token != NULL && sensitive != NULL && extractable != NULL) {
-                    // Non-token, sensitive and extractable key, we need
-                    // to prevent an opaque SunPKCS11 P11Key object, which
-                    // refrains from obtaining the sensitive attributes.
+            }
+            if (token != NULL && sensitive != NULL && extractable != NULL) {
+                // We know that *token == CK_FALSE && *sensitive == CK_TRUE.
+                if (*extractable == CK_TRUE) {
+                    // Make the key look as non-sensitive so OpenJDK does not
+                    // consider it opaque and gets the sensitive attribute
+                    // values that the exporter will handle.
                     dbg_trace("Extractable key, forcing CKA_SENSITIVE=CK_FALSE "
-                              "to avoid opaque P11Key objects");
+                              "to avoid an opaque P11Key object");
                     *sensitive = CK_FALSE;
-                    break;
+                } else {
+                    dbg_trace("Let non-extractable key be handled as opaque");
                 }
             }
         }
     } else if (ret == CKR_ATTRIBUTE_SENSITIVE) {
         bool first_call = !cached_attrs_initialized;
-        if (first_call && export_and_store_key_in_tls(key_class, key_type,
-                                                      key_id) != CKR_OK) {
-            return_with_cleanup(CKR_GENERAL_ERROR);
+        if (first_call) {
+            ret = export_and_store_key(key_class, key_type, key_id);
+            if (ret != CKR_OK) {
+                return_with_cleanup(ret);
+            }
         }
         CK_ATTRIBUTE_PTR cached_attr = NULL;
         for (size_t n = 0; n < n_attributes; n++) {
+            dbg_trace_attr("Attribute returned by NSS C_GetAttributeValue()",
+                           attributes[n]);
             if (attributes[n].ulValueLen == CK_UNAVAILABLE_INFORMATION) {
                 cached_attr = get_sensitive_cached_attr(attributes[n].type);
                 if (cached_attr == NULL) {
@@ -338,19 +364,18 @@ CK_RV export_key(CK_OBJECT_CLASS key_class, CK_KEY_TYPE key_type,
                 }
                 if (first_call) {
                     // First call, libj2pkcs11 is querying the buffer sizes.
-                    dbg_trace("First call, replacing ulValueLen = CK_"
-                              "UNAVAILABLE_INFORMATION with ulValueLen = %lu",
-                              cached_attr->ulValueLen);
                     attributes[n].ulValueLen = cached_attr->ulValueLen;
+                    dbg_trace_attr("First call, replaced ulValueLen",
+                                   attributes[n]);
                 } else {
                     // Second call, libj2pkcs11 has allocated the buffers and
                     // is trying to retrieve the attribute values.
-                    dbg_trace_attr("Second call, copying previously exported "
-                                   "attribute",
-                                   *cached_attr);
                     attributes[n].ulValueLen = cached_attr->ulValueLen;
                     memcpy(attributes[n].pValue, cached_attr->pValue,
                            attributes[n].ulValueLen);
+                    dbg_trace_attr("Second call, copied previously exported "
+                                   "attribute",
+                                   attributes[n]);
                 }
             }
         }
